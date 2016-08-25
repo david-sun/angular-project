4c0524f148b6309c489197eeae63bf7926a73fbd
Liqun Sun <dsun@highroads.com>
2016-08-25 01:55:21 -0400

    duplicate function

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -3,12 +3,21 @@
 angular.module('myApp.view1', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
-  });
+    $routeProvider.when('/view1', {
+        templateUrl: 'view1/view1.html',
+        controller: 'View1Ctrl'
+    });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', ['$scope', function($scope) {
+    $scope.myArray = [1, 2, 3, 4, 5];
 
+    // it is better to put this modifing standard object function in a seperate service
+    Array.prototype.duplicate = function() {
+        Array.prototype.push.apply(this, this);
+    }
+
+    $scope.duplicate = function() {
+        $scope.myArray.duplicate();
+    }
 }]);
