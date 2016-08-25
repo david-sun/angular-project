f2398292821a3d43b0b7d9aea495ee43408bcc66
Liqun Sun <dsun@highroads.com>
2016-08-25 09:55:10 -0400

    use random number to controll cache

--- app/service/http-log-svc.js
+++ app/service/http-log-svc.js
@@ -8,6 +8,10 @@
                     return $filter('date')(new Date(), 'yyyy-MM-dd HH:mm:ss.sss');
                 }
 
+                function addRandomNumber(url) {
+                    return url + ((url.indexOf('?') === -1) ? '?' : '&') + 'rm=' + (Math.random() * 10000000).toFixed(0);
+                }
+
                 // This is just a example, you do not have to keep 4 of following functions
                 // in practice, we just implement the one we needed
                 return {
@@ -17,7 +21,9 @@
                         // you can add extra header info
                         config.headers.Authorization = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
                         // you can disable the cache
-                        config.cache = false;
+                        // config.cache = false;
+                        // Or use random number to disable cache
+                        config.url = addRandomNumber(config.url);
                         return config || $q.when(config);
                     },
                     requestError: function(rejection) {
