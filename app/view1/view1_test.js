29d0caec4b0d1baca710d102d8985a5bb00c06f6
Liqun Sun <dsun@highroads.com>
2016-08-25 02:00:17 -0400

    fix the unit test failure

--- app/view1/view1_test.js
+++ app/view1/view1_test.js
@@ -2,15 +2,17 @@
 
 describe('myApp.view1 module', function() {
 
-  beforeEach(module('myApp.view1'));
+    beforeEach(module('myApp.view1'));
 
-  describe('view1 controller', function(){
+    describe('view1 controller', function() {
 
-    it('should ....', inject(function($controller) {
-      //spec body
-      var view1Ctrl = $controller('View1Ctrl');
-      expect(view1Ctrl).toBeDefined();
-    }));
+        it('should ....', inject(function($rootScope, $controller) {
+            //spec body
+            var view1Ctrl = $controller('View1Ctrl', {
+                $scope: $rootScope.$new()
+            });
+            expect(view1Ctrl).toBeDefined();
+        }));
 
-  });
+    });
 });
